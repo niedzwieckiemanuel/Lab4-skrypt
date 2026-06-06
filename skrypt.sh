679c86a5050dfd00771b5c24b45115031cb0b06c
Emanuel <dsw62133@student.ideis.pl>
2026-06-06 16:00:26 +0200

    Dodano argument liczbowy do flagi logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,8 @@
 if [ "$1" == "--date" ]; then
     date
 elif [ "$1" == "--logs" ]; then
-    for i in {1..100}; do
+    count=${2:-100}
+    for i in $(seq 1 $count); do
         echo -e "Nazwa pliku: log${i}.txt\nSkrypt: $0\nData: $(date)" > log${i}.txt
     done
 fi
