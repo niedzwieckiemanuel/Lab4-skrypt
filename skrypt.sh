9b8d5e35fad0643caaf0e442b58b79e210a825d8
Emanuel <dsw62133@student.ideis.pl>
2026-06-06 16:00:04 +0200

    Dodano tworzenie 100 plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    for i in {1..100}; do
+        echo -e "Nazwa pliku: log${i}.txt\nSkrypt: $0\nData: $(date)" > log${i}.txt
+    done
 fi
